f56a415d31bbee82de984f3b44e0198a9e137731
dizzle2003 <ajaliedennis@yahoo.com>
2020-04-19 19:26:37 +0100

    Created PORT and configured environmental variable

--- server.js
+++ server.js
@@ -3,43 +3,51 @@ const bodyParser = require('body-parser');
 const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
 const knex = require('knex');
-
+const PORT = process.env.PORT || 3001;
 
 const register = require('./Controllers/register');
 const signin = require('./Controllers/signin');
 const profile = require('./Controllers/profile');
 const image = require('./Controllers/image');
 
-
-
-
 const db = knex({
   client: 'pg',
   connection: {
-    host : '127.0.0.1',
-    user : 'postgres',
-    password : 'test1234',
-    database : 'smartbrains'
-  }
+    host: '127.0.0.1',
+    user: 'postgres',
+    password: 'test1234',
+    database: 'smartbrains',
+  },
 });
 
 const app = express();
 
-app.use(cors())
+app.use(cors());
 app.use(bodyParser.json());
 
-app.get('/', (req, res)=> {res.send('it is working'); })
+app.get('/', (req, res) => {
+  res.send('it is working');
+});
 
-app.post('/signin', (req, res) => {signin.handleSignin (req, res, db, bcrypt) } )
+app.post('/signin', (req, res) => {
+  signin.handleSignin(req, res, db, bcrypt);
+});
 
-app.post('/register', (req, res) => {register.handleRegister (req, res, db, bcrypt) } )
+app.post('/register', (req, res) => {
+  register.handleRegister(req, res, db, bcrypt);
+});
 
-app.get('/profile/:id', (req, res) => {profile.handleProfile (req, res, db ) } )
+app.get('/profile/:id', (req, res) => {
+  profile.handleProfile(req, res, db);
+});
 
-app.put('/image', (req, res) => {image.handleImage (req, res, db ) })
-app.post('/imageurl', (req, res) => { image.handleApiCall(req, res)})
+app.put('/image', (req, res) => {
+  image.handleImage(req, res, db);
+});
+app.post('/imageurl', (req, res) => {
+  image.handleApiCall(req, res);
+});
 
-
-app.listen(process.env.PORT || 3001, ()=> {
-  console.log(`app is running on port ${process.env.PORT}`);
-})
+app.listen(PORT, () => {
+  console.log(`App is running on port ${PORT}`);
+});
