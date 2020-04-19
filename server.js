9018e42b97fe2498cd2c86b088b6e0c96ef09a7b
dizzle2003 <ajaliedennis@yahoo.com>
2020-04-19 23:15:23 +0100

    fixed the issue

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const app = express();
 const bodyParser = require('body-parser');
 const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
 const knex = require('knex');
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3001
+
 
 const register = require('./Controllers/register');
 const signin = require('./Controllers/signin');
@@ -20,7 +22,8 @@ const db = knex({
   },
 });
 
-const app = express();
+
+
 
 app.use(cors());
 app.use(bodyParser.json());
